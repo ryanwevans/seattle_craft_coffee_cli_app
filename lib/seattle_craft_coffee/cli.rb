d5306f7f221962a5566c9ffa1a4d31c6785af571
ryanwevans <ryanwevans@icloud.com>
2017-08-04 10:39:28 -0700

    added class to scraper.rb, added class and initial methods to cli.rb

---
+++ lib/seattle_craft_coffee/cli.rb
@@ -0,0 +1,20 @@
+class SeattleCraftCoffee::Brewers
+
+  def call
+    greeting
+    menu
+    goodbye
+  end
+
+  def greeting
+    puts "Welcome to Seattle's Top Craft Coffee"
+  end
+
+  def menu
+    puts "The menu will be here soon!"
+  end
+
+  def goodbye
+    puts "Enjoy your craft coffee!  Goodbye..."
+
+end
---
+++ lib/seattle_craft_coffee/scraper.rb
@@ -0,0 +1,5 @@
+class SeattleCraftCoffee::Scraper
+
+  
+
+end
