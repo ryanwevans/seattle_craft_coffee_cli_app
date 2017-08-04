2d27059458d6e9aad9cffcc1d3bbdb28acac87b0
ryanwevans <ryanwevans@icloud.com>
2017-08-04 10:51:44 -0700

    added simple user interaction to #menu

--- lib/seattle_craft_coffee/cli.rb
+++ lib/seattle_craft_coffee/cli.rb
@@ -11,7 +11,19 @@ class SeattleCraftCoffee::Brewers
   end
 
   def menu
-    puts "The menu will be here soon!"
+    puts "Which Craft Coffee brewer are you interested in? Enter the number:"
+    input = gets.to_i
+    case input
+    when 1
+      puts "you chose brewer number 1"
+    when 2
+      puts "you chose brewer number 2"
+    when 3
+      puts "you chose brewer number 3"
+    else
+      puts "sorry that wasn't a valid choice"
+    end
+
   end
 
   def goodbye
