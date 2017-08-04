20ef9fc2215cb3e0d73cd5dfb6bfe2c523a5becd
ryanwevans <ryanwevans@icloud.com>
2017-08-04 10:29:24 -0700

    added requirements to environment file

--- lib/seattle_craft_coffee.rb
+++ lib/seattle_craft_coffee.rb
@@ -1,4 +1,10 @@
-require "seattle_craft_coffee/version"
+require_relative "./seattle_craft_coffee/scraper"
+require_relative "./seattle_craft_coffee/cli"
+require_relative "./seattle_craft_coffee/version"
+
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
 
 module SeattleCraftCoffee
   # Your code goes here...
