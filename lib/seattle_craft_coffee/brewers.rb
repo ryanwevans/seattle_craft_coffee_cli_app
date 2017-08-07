764ebbaf2610439e8fd85f10269482468860dc8a
ryanwevans <ryanwevans@icloud.com>
2017-08-07 11:36:41 -0700

    corrected the name scraping code

--- lib/seattle_craft_coffee/brewers.rb
+++ lib/seattle_craft_coffee/brewers.rb
@@ -5,6 +5,7 @@ class SeattleCraftCoffee::Brewers
 
   def initialize(name, neighborhood, description)
     @name = name
+    @neighborhood = neighborhood
     @description = description
     @@all << self
   end
@@ -12,7 +13,7 @@ class SeattleCraftCoffee::Brewers
   def self.add_brewers
     SeattleCraftCoffee::Scraper.scrape_page.each do |brewer|
       self.new(
-      brewer.css("h1 save-venue__title.font--h1").text,
+      brewer.css("a.save-venue__link").text,
       brewer.css("h2.save-venue__neighborhood.font--h4").text,
       brewer.css("p.save-venue__description.font--body").text
       )
