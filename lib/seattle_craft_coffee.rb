0cdcce99305a69703259f9fdb46ce06780275137
ryanwevans <ryanwevans@icloud.com>
2017-08-10 17:10:59 -0700

    refactored these files because app wasn't calling cli file; last commit, again :)

--- lib/seattle_craft_coffee.rb
+++ lib/seattle_craft_coffee.rb
@@ -1,12 +1,8 @@
-require_relative "./seattle_craft_coffee/scraper"
-require_relative "./seattle_craft_coffee/cli"
-require_relative "./seattle_craft_coffee/version"
-require_relative "./seattle_craft_coffee/brewers"
-
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
-module SeattleCraftCoffee
-  # Your code goes here...
-end
+require_relative "./seattle_craft_coffee/scraper"
+require_relative "./seattle_craft_coffee/cli"
+require_relative "./seattle_craft_coffee/version"
+require_relative "./seattle_craft_coffee/brewers"
