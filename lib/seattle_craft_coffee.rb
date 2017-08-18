c98a3d669d2619f3ffce3642c2e281de15a5706a
ryanwevans <ryanwevans@icloud.com>
2017-08-17 19:40:03 -0700

    cleaned up gemspec, lib/seattle_craft_coffee, bin/seattle_craft_coffee

--- lib/seattle_craft_coffee.rb
+++ lib/seattle_craft_coffee.rb
@@ -2,7 +2,10 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
-require_relative "./seattle_craft_coffee/scraper"
-require_relative "./seattle_craft_coffee/cli"
-require_relative "./seattle_craft_coffee/version"
-require_relative "./seattle_craft_coffee/brewers"
+require_relative "seattle_craft_coffee/version"
+require_relative "seattle_craft_coffee/cli"
+require_relative "seattle_craft_coffee/scraper"
+require_relative "seattle_craft_coffee/brewers"
+
+module SeattleCraftCoffee
+end
--- seattle_craft_coffee.gemspec
+++ seattle_craft_coffee.gemspec
@@ -23,8 +23,9 @@ Gem::Specification.new do |spec|
 
   spec.files         = `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
   spec.bindir        = "exe"
-  # spec.executables << 'seattle_craft_coffee'
+
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
+  # spec.executables = ["seattle_craft_coffee"]
   spec.require_paths = ["lib"]
 
   spec.add_development_dependency "bundler", "~> 1.12"
